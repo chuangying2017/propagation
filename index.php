db868d1195ba676010f23f1fd3cee1a73625e3ff
张志伟 <631711487@qq.com>
2020-07-04 20:53:48 +0800

    更新propagation传播记录

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 //mongodb://su:qwer+123@api.data.shalangzhen.cn
-$collection = (new \MongoDB\Client('mongodb://192.168.56.33'));
 
 
 class PropagationModel extends \MongoDB\Collection
@@ -10,7 +9,7 @@ class PropagationModel extends \MongoDB\Collection
     protected $collectionName = 'promotion';
     public function __construct()
     {
-        $manager = (new \MongoDB\Client('mongodb://su:qwer+123@api.data.shalangzhen.cn'));
+        $manager = (new \MongoDB\Client('mongodb://192.168.56.33'));
         parent::__construct($manager->getManager(), 'datacenter', $this->collectionName);
     }
 }
@@ -19,7 +18,6 @@ function update($id='', $total=0)
 {
     print date('H:i:s') . " ...new start done " . $total . PHP_EOL;
     $propagationModel = new PropagationModel();
-    $currentTime = time() + 599;
     $pipeline = [
         array('$group'      => array('_id' => '$activity_id', 'total'=> array('$sum'=>1),
             'promotions' => array('$push' => ['id'=>'$_id','activity_id'=>'$activity_id','time'=>'$time']))),
@@ -30,11 +28,12 @@ function update($id='', $total=0)
             'activity_id'       => '$promotions.activity_id',
             'time'              => '$promotions.time',
         ) ),
+        array('$sort' => ['_id' => 1]),
         array('$limit' => 3000)
     ];
     if (!empty($id))
     {
-        array_unshift($pipeline, ['$match' => ['$gt' => new $id]]);
+        array_unshift($pipeline, ['$match' => ['_id'=> ['$gt' => $id]]]);
     }
     $res = $propagationModel->aggregate(
         $pipeline,
@@ -43,19 +42,12 @@ function update($id='', $total=0)
             'readPreference' => new \MongoDB\Driver\ReadPreference(\MongoDB\Driver\ReadPreference::PRIMARY)
         ]);
     $i = 0;
+    unset($id);
     foreach ($res as $value)
     {
-            if ($currentTime <= time())
-            {
-                $i = update($value['_id'], $i);
-                if ($i)
-                {
-                    break;
-                }
-            }
-
+            $id = $value['_id'];
             try {
-                $propagationModel->updateOne(['_id' => $value['_id']], ['$set' => ['counters' => $value['counter']]]);
+                $propagationModel->updateOne(['_id' => $id], ['$set' => ['counters' => $value['counter']]]);
             }catch (\Exception $exception)
             {
                 print 000;
@@ -63,6 +55,10 @@ function update($id='', $total=0)
                 continue;
             }
             $i += 1;
+            if ($i % 100 == 0)
+            {
+                print $i . "  current number...".PHP_EOL;
+            }
     }
 
     if ($i<1)
@@ -70,7 +66,14 @@ function update($id='', $total=0)
         return $i;
     }
 
-    return update($value['_id'], $i);
+    if (isset($id))
+    {
+        return update($id, $i);
+    }
 }
 
-print date('H:i:s') . " ...new start done ".PHP_EOL;
+$res = update();
+
+print PHP_EOL;
+
+var_dump($res);
